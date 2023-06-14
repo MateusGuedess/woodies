0a15b63325b31060f0b067188e35502e250d73e9
Mateus Guedes <mxteusgued@gmail.com>
2023-06-13 23:08:21 -0300

    feat: organize structure

---
+++ src/components/AboutUs/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "./style";
+
+function AboutUs() {
+  return (
+    <Container>
+      <div>aboutus</div>
+      <div>woodies</div>
+      <div>made just for us</div>
+    </Container>
+  );
+}
+
+export default AboutUs;
